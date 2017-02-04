18bcecf1c9e1d6565dfaf24bf8394ecc27e561d7
Christopher Trom <chtrom@gmail.com>
2017-02-04 13:07:36 -0600

    Added Spring Boot.

--- src/main/java/fedor/games/werewolf/Application.java
+++ src/main/java/fedor/games/werewolf/Application.java
@@ -1,10 +1,17 @@
 package fedor.games.werewolf;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * The jumping off place.
  */
+@SpringBootApplication
+@PropertySource(value = {"classpath:application.properties"})
 public class Application {
 	public static void main (String args[]) {
 		System.out.println("Hello!");
+		SpringApplication.run(Application.class, args);
 	}
 }
