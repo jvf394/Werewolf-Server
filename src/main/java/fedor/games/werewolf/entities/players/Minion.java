f4a9bcb10abc408cd60bf51e57ee7adc68b02086
Christopher Trom <chtrom@gmail.com>
2017-10-28 11:45:17 -0500

    Add new experimental class structure for players.

---
+++ src/main/java/fedor/games/werewolf/entities/players/Minion.java
@@ -0,0 +1,13 @@
+package fedor.games.werewolf.entities.players;
+
+/**
+ *
+ */
+class Minion extends Player {
+	@Override
+	void takeTurn() {
+		// Perform turn actions.
+
+		// Reveal werewolves
+	}
+}
